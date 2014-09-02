31c676176c6f8c844ab7ca8ea45356972152910e
Ben Polidore <ben.polidore@itg.com>
2014-09-02 17:12:28 -0400

    taking some shape

--- src/loadJsons.hpp
+++ src/loadJsons.hpp
@@ -1,11 +1,15 @@
 #include <string>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/filesystem.hpp>
 
 using namespace std;
+using namespace boost::filesystem;
+using boost::property_tree::ptree;
+using boost::property_tree::read_json;
+using boost::property_tree::write_json;
 
 namespace dfg {
-  class LoadJsons {
-    string path;
-    LoadJsons(p) : path(p) {}
-    public vector<
-  }
+  //let us use a function, not a method
+  public static 
 }
