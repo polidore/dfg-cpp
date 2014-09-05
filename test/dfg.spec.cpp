2e0d579f0d1df351a9b2200be3c0a94448dbbf16
Ben Polidore <ben.polidore@itg.com>
2014-09-05 08:18:55 -0400

    clean up tests

--- test/dfg.spec.cpp
+++ test/dfg.spec.cpp
@@ -2,21 +2,7 @@
 #include "catch.hpp"
 #include "../src/loadJsons.hpp"
 
-/*
-unsigned int Factorial(unsigned int number) {
-  return number > 1 ? Factorial(number-1)*number : 1;
-}
-
-TEST_CASE("Factorials are computed", "[factorial]") {
-  REQUIRE(Factorial(0) == 1);
-  REQUIRE(Factorial(1) == 1);
-  REQUIRE(Factorial(2) == 2);
-  REQUIRE(Factorial(3) == 6);
-  REQUIRE(Factorial(10) == 3628800);
-}
-*/
-
-TEST_CASE("Should find no jsons", "[json]") { 
+TEST_CASE("Should load basic json data", "[json]") { 
   auto jsons = dfg::loadJsons("./");
   REQUIRE(jsons.size() == 3);
   REQUIRE(jsons[0].get<int>("voltage") == 110);
