cb85278c112152454ec6f8a30ec1c21d88b1ca08
Ben Polidore <ben.polidore@itg.com>
2015-03-17 09:33:57 -0400

    fix weird bug in tests

--- test/dfg.spec.cpp
+++ test/dfg.spec.cpp
@@ -6,12 +6,12 @@ TEST_CASE("Should load basic json data", "[json]") {
   auto jsons = dfg::loadJsons("./");
   REQUIRE(jsons.size() == 3);
   int numOverride = 0;
-  int numVoltage = 1;
+  int numVoltage = 0;
   for(auto j : jsons) {
     REQUIRE(j.get<std::string>("@type") == "electricity");
     numOverride += j.count("@override");
     numVoltage += j.count("voltage");
   }
   REQUIRE(numOverride == 2);
-  REQUIRE(numVoltage == 1);
+  REQUIRE(numVoltage == 2);
 }
